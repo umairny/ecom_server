423cb1063d49c57278677b453e359ae787d6c8ad
Umair Ahmad <umairny1@gmail.com>
2023-02-22 02:40:49 -0500

    change wsgi

--- server/wsgi.py
+++ server/wsgi.py
@@ -1,23 +1,7 @@
-# This file contains the WSGI configuration required to serve up your
-# web application at http://umairahmad.pythonanywhere.com/
-# It works by setting the variable 'application' to a WSGI handler of some
-# description.
-#
-# The below has been auto-generated for your Django project
-
 import os
-import sys
 
-# add your project directory to the sys.path
-project_home = "/home/umairahmad/ecom_server"
-if project_home not in sys.path:
-    sys.path.insert(0, project_home)
-
-# set environment variable to tell django where your settings.py is
-os.environ["DJANGO_SETTINGS_MODULE"] = "server.settings"
-
-
-# serve django via WSGI
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
 
 application = get_wsgi_application()
