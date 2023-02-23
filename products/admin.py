1e2d8597d32f5de3360d8920f7716601316f66a9
Umair Ahmad <umairny1@gmail.com>
2023-02-23 02:41:50 -0500

    image add registry

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from products.models import Products
+from products.models import Products, Images
 
 # Register your models here.
 admin.site.register(Products)
+admin.site.register(Images)
